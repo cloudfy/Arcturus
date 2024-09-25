bdcdeff021c6ae7f4026e6fd20e636db887da77b
Cloudfy <rasmus@foged.org>
2024-09-25 09:00:29 +0200

    Adding files and dependencies

--- src/Arcturus.ResultObjects/ResultExtensions.cs
+++ src/Arcturus.ResultObjects/ResultExtensions.cs
@@ -4,12 +4,29 @@ namespace Arcturus.ResultObjects;
 
 public static class ResultExtensions
 {
+    /// <summary>
+    /// Assigns the <paramref name="exception"/> to the result.
+    /// <para>
+    /// <b>Result object will be marked as failure.</b>
+    /// </para>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="value">Required.</param>
+    /// <param name="exception"></param>
+    /// <returns><see cref="Result{T}"/></returns>
     public static Result<T> WithException<T>(this Result<T> value, Exception exception)
     {
         value.Exception = exception;
         value.IsSuccess = false;
         return value;
     }
+    /// <summary>
+    /// Assigns the <paramref name="httpStatusCode"/> to the result.
+    /// </summary>
+    /// <typeparam name="T">Type of result object.</typeparam>
+    /// <param name="value">Required.</param>
+    /// <param name="httpStatusCode"><see cref="HttpStatusCode"/> to assign.</param>
+    /// <returns><see cref="Result{T}"/></returns>
     public static Result<T> WithHttpStatusCode<T>(this Result<T> value, HttpStatusCode httpStatusCode)
     {
         value.HttpStatusCode = httpStatusCode;
--- src/Arcturus.ResultObjects/ResultOfT.cs
+++ src/Arcturus.ResultObjects/ResultOfT.cs
@@ -1,11 +1,27 @@
 ﻿namespace Arcturus.ResultObjects;
 
+/// <summary>
+/// Represents a result of an operation - success or failure.
+/// </summary>
+/// <typeparam name="T">Type fo containing result.</typeparam>
 public sealed class Result<T> : Result
 {
-    internal Result(bool isSuccess, T? value) : base(isSuccess)
+    internal Result(bool isSuccess, T? value, Fault? fault = null) : base(isSuccess, fault)
     {
         Value = value;
     }
-
+    /// <summary>
+    /// Returns value of result or null.
+    /// </summary>
     public T? Value { get; }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="value"></param>
+    public static implicit operator Result<T>(T? value) => new(true, value);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="failure"></param>
+    public static implicit operator Result<T>(Fault failure) => new(false, default, failure);
 }
