1a3851f6975b6b176fe4ccb806d1269ad50dde2d
Cloudfy <rasmus@foged.org>
2024-10-03 12:59:28 +0200

    Adding endpoint modules

---
+++ src/Arcturus.AspNetCore.Endpoints/IEndPointModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Arcturus.AspNetCore.Endpoints;
+
+/// <summary>
+/// Defines an endpoint module.
+/// </summary>
+public interface IEndPointModule
+{
+    /// <summary>
+    /// Adds an <see cref="IEndPointModule"/> route to the <see cref="IEndpointRouteBuilder">.
+    /// </summary>
+    /// <param name="app">Required.</param>
+    void AddRoute(IEndpointRouteBuilder app);
+}
