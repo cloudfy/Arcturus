3001faa179cb22a6d3a7d12150f3c85727ed7c6c
Cloudfy <rasmus@foged.org>
2024-10-23 14:10:18 +0200

    Adding XML

--- src/Arcturus.ResultObjects/Specialized/AmbiguousFault.cs
+++ src/Arcturus.ResultObjects/Specialized/AmbiguousFault.cs
@@ -2,6 +2,11 @@
 
 namespace Arcturus.ResultObjects.Specialized;
 
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Code"></param>
+/// <param name="Message"></param>
 public record AmbiguousFault(string Code, string Message)
     : Fault(Code, Message), ISpecializedFault
 {
