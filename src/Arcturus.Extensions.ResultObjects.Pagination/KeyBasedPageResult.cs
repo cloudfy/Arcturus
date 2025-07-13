922acf73cd3c68dcb1122120fb8432f61dadb2ca
Cloudfy <rasmus@foged.org>
2025-07-13 15:22:48 +0200

    Add pagination project and update solution structure
    - Introduced `Arcturus.Extensions.ResultObjects.Pagination` project with unique GUID and build configurations.
    - Removed a project reference from `Arcturus.Data.Repository.EntityFrameworkCore.csproj`.
    - Created `Arcturus.Extensions.ResultObjects.Pagination.csproj` with package metadata and a reference to `Arcturus.ResultObjects`.
    - Added `KeyBasedPageResult<T, TKey>` and `PagedResult<T, TKey>` classes for handling paginated results, including JSON serialization attributes.

---
+++ src/Arcturus.Extensions.ResultObjects.Pagination/KeyBasedPageResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Arcturus.ResultObjects.Pagination;
+
+public sealed class KeyBasedPageResult<T, TKey> 
+    : Result<PagedResult<T, TKey>>
+{
+    internal KeyBasedPageResult(
+        bool isSuccess, PagedResult<T, TKey> pagedResult, Fault? fault = null) 
+        : base(isSuccess, pagedResult, fault)
+    {
+    }
+
+    public static KeyBasedPageResult<T, TKey> FromArray(
+        T[] results, long totalCount, TKey? nextToken, int pageSize = 20)
+    {
+        var value = new PagedResult<T, TKey>(results, totalCount, nextToken, pageSize);
+        return new KeyBasedPageResult<T, TKey>(true, value);
+    }
+}
---
+++ src/Arcturus.Extensions.ResultObjects.Pagination/PagedResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Arcturus.ResultObjects.Pagination;
+
+public sealed class PagedResult<T, TKey>
+{
+    internal PagedResult(T[] results, long totalCount,TKey? nextToken, int pageSize = 20)
+    {
+        Results = [.. results.Take(pageSize)];
+        TotalCount = totalCount;
+        HasMore = results.Length > pageSize;
+        NextToken = nextToken;
+    }
+    public T[] Results { get; } = [];
+    public long TotalCount { get; }
+    public bool HasMore { get; }
+    public TKey? NextToken { get; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public Dictionary<string, string>? Links { get; set; }
+}
