8573012b5625fbb69aec19cf12b4ccbac0dbdd82
Cloudfy <rasmus@foged.org>
2025-08-11 13:20:48 +0200

    Enhance event bus queue handling and documentation
    Updated IEventBusFactory to clarify default queue behavior.
    Modified SqliteEventBusFactory to use DefaultQueueName
    when the queue parameter is null. Added DefaultQueueName
    property to SqliteEventBusOptions for improved configuration
    flexibility.

--- src/Arcturus.EventBus.Abstracts/IEventBusFactory.cs
+++ src/Arcturus.EventBus.Abstracts/IEventBusFactory.cs
@@ -8,13 +8,13 @@ public interface IEventBusFactory
     /// <summary>
     /// Creates a new instance of <see cref="IProcessor"/>. Processors are used to process messages from the event bus asynchronously.
     /// </summary>
-    /// <param name="queue">Optional. Name of the queue.</param>
+    /// <param name="queue">Optional. Name of the queue. Defaults to default_queue.</param>
     /// <returns><see cref="IProcessor"/> instance.</returns>
     IProcessor CreateProcessor(string? queue = null);
     /// <summary>
     /// Creates a new instance of <see cref="IPublisher"/>. Publishers are used to publish messages to the event bus.
     /// </summary>
-    /// <param name="queue">Optional. Name of the queue.</param>
+    /// <param name="queue">Optional. Name of the queue. Defaults to default_queue.</param>
     /// <returns><see cref="IPublisher"/> instance.</returns>
     IPublisher CreatePublisher(string? queue = null);
     //ISubscriber CreateSubscriber();
