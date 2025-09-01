2ea08be71c1bb37bdc089ba8d5073300e47f17e3
Cloudfy <rasmus@foged.org>
2025-09-01 08:53:49 +0200

    Add Arcturus.Xunit project and enhance package metadata
    - Updated `Arcturus.sln` to include the new `Arcturus.Xunit` project.
    - Added `PackageIconUrl` and detailed `Description` to several projects:
      - `Arcturus.AspNetCore.Endpoints`
      - `Arcturus.Extensions.Repository.Pagination`
      - `Arcturus.Extensions.ResultObjects.AspNetCore`
      - `Arcturus.ResultObjects`
    - Created `Arcturus.Xunit.csproj` with necessary metadata and dependencies.
    - Introduced `TestHost` class for improved testing setup in ASP.NET Core.

---
+++ src/Arcturus.Xunit/TestHost.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Arcturus.Xunit;
+
+public abstract class TestHost : IClassFixture<TestHost>
+{
+    private readonly CancellationTokenSource _cancellationTokenSource;
+
+    public TestHost()
+    {
+        _cancellationTokenSource = new CancellationTokenSource();
+
+        var hostBuilder = Host.CreateDefaultBuilder();
+        var host = hostBuilder
+            .ConfigureServices((context, services) =>
+            {
+                ConfigureServices(services, context.Configuration);
+            })
+            .Build();
+
+        ServiceProvider = host.Services;
+    }
+    public abstract void ConfigureServices(IServiceCollection services, IConfiguration configuration);
+    public IServiceProvider ServiceProvider { get; }
+    public CancellationToken CancellationToken => _cancellationTokenSource.Token;
+    public T GetRequiredService<T>(ITestOutputHelper testOutputHelper)
+    {
+        var service = ServiceProvider.GetService<T>();
+        if (service is null)
+            throw new ArgumentNullException("Service required but is null");
+        return service!;
+    }
+}
